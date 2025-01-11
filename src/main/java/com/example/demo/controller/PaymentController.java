34df65b4b4fde7464d98098520c9a04e8b1b07db
Kmmohan <mohankmkk65@gmail.com>
2025-01-11 22:11:47 +0530

    paymentcontroller.java

--- src/main/java/com/example/demo/controller/PaymentController.java
+++ src/main/java/com/example/demo/controller/PaymentController.java
@@ -1,12 +1,19 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Payment;
 import com.example.demo.service.PaymentService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/v1/payments")
@@ -37,7 +44,7 @@ public class PaymentController {
     }
 
     // Update Payment
-    @PutMapping("/{id}")
+     @PutMapping("/{id}")
     public Payment updatePayment(@PathVariable Long id, @RequestBody Payment updatedPayment) {
         return paymentService.updatePayment(id, updatedPayment);
     }
